8811de927a270827dd47639dccdbd66545216268
zulfanurrifaat <zullfanurrifaat@gmail.com>
2024-03-09 02:15:34 +0700

    create page home view

--- lib/app/modules/add_product/views/add_product_view.dart
+++ lib/app/modules/add_product/views/add_product_view.dart
@@ -5,19 +5,75 @@ import 'package:get/get.dart';
 import '../controllers/add_product_controller.dart';
 
 class AddProductView extends GetView<AddProductController> {
-  const AddProductView({Key? key}) : super(key: key);
+  AddProductView({Key? key}) : super(key: key);
+  final TextEditingController codeC = TextEditingController();
+  final TextEditingController nameC = TextEditingController();
+  final TextEditingController qtyC = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AddProductView'),
+        title: const Text('ADD PRODUCT'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'AddProductView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            autocorrect: false,
+            controller: codeC,
+            keyboardType: TextInputType.number,
+            maxLength: 10,
+            decoration: InputDecoration(
+              labelText: "Product Code",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            autocorrect: false,
+            controller: nameC,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              labelText: "Product Name",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            autocorrect: false,
+            controller: qtyC,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Quantity",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+            ),
+          ),
+          const SizedBox(height: 35),
+          ElevatedButton(
+            onPressed: () async {
+              if (controller.isLoading.isFalse) {
+                controller.isLoading(true);
+                //Map<String, dynamic> hasil = ....
+                controller.isLoading(false);
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(9),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 20),
+            ),
+            child: Obx(() => Text(
+                controller.isLoading.isFalse ? "ADD PRODUCT" : "LOADING...")),
+          ),
+        ],
       ),
     );
   }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,28 +1,84 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:qrscanner/app/controllers/auth_controller.dart';
 import 'package:qrscanner/app/routes/app_pages.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   HomeView({Key? key}) : super(key: key);
+  final AuthController authC = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
-    final AuthController authC = Get.find<AuthController>();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: GridView.builder(
+        itemCount: 4,
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
         ),
+        itemBuilder: (context, index) {
+          late String title;
+          late IconData icon;
+          late VoidCallback onTap;
+
+          switch (index) {
+            case 0:
+              title = "Add Product";
+              icon = Icons.post_add_rounded;
+              onTap = () => Get.toNamed(Routes.addProduct); // Perbaikan disini
+              break;
+            case 1:
+              title = "Products";
+              icon = Icons.list_alt_outlined;
+              onTap = () => Get.toNamed(Routes
+                  .products); // Anda dapat menambahkan logic onTap jika diperlukan
+              break;
+            case 2:
+              title = "QR Code";
+              icon = Icons.qr_code;
+              onTap = () {
+                print("OPEN CAMERA");
+              }; // Anda dapat menambahkan logic onTap jika diperlukan
+              break;
+            case 3:
+              title = "Catalog";
+              icon = Icons.document_scanner_outlined;
+              onTap = () {
+                print("OPEN PDF");
+              }; // Anda dapat menambahkan logic onTap jika diperlukan
+              break;
+            // Atur default icon ke sini jika diperlukan
+          }
+
+          return Material(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(9),
+            child: InkWell(
+              onTap: onTap,
+              borderRadius: BorderRadius.circular(9),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Icon(icon,
+                        size: 50), // Menggunakan icon yang telah diinisialisasi
+                  ),
+                  const SizedBox(height: 10),
+                  Text(title), // Menggunakan title yang telah diinisialisasi
+                ],
+              ),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
