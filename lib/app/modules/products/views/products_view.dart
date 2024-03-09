40ae52f62c035ffece8a0f0388b079c4489ab65c
zulfanurrifaat <zullfanurrifaat@gmail.com>
2024-03-09 23:59:20 +0700

    All Product

--- lib/app/modules/products/views/products_view.dart
+++ lib/app/modules/products/views/products_view.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qrscanner/app/data/models/product_model.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/products_controller.dart';
 
 class ProductsView extends GetView<ProductsController> {
@@ -10,14 +13,84 @@ class ProductsView extends GetView<ProductsController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ProductsView'),
+        title: const Text('PRODUCTS'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ProductsView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: controller.streamProducts(),
+        builder: (context, snapProducts) {
+          if (snapProducts.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapProducts.data!.docs.isEmpty) {
+            return const Center(
+              child: Text("No Products"),
+            );
+          }
+
+          List<ProductModel> allProducts = [];
+
+          for (var element in snapProducts.data!.docs) {
+            allProducts.add(ProductModel.fromJson(element.data()));
+          }
+
+          return ListView.builder(
+            itemCount: allProducts.length, // perbaikan di sini
+            padding: const EdgeInsets.all(20),
+            itemBuilder: (context, index) {
+              ProductModel product = allProducts[index];
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.only(bottom: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Get.toNamed(Routes.detailProduct);
+                  },
+                  borderRadius: BorderRadius.circular(9),
+                  child: Container(
+                    height: 100,
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.code,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(product.name),
+                              Text("Jumlah : ${product.qty}"),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 50,
+                          width: 50,
+                          child: QrImageView(
+                            data: product.code,
+                            size: 200.0,
+                            version: 5,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
