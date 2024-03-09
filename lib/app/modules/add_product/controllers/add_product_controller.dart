5570c5349aa87e9a36cd61a3850093e4021a6ee7
zulfanurrifaat <zullfanurrifaat@gmail.com>
2024-03-09 18:30:04 +0700

    add Product

--- lib/app/modules/add_product/controllers/add_product_controller.dart
+++ lib/app/modules/add_product/controllers/add_product_controller.dart
@@ -1,5 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
 class AddProductController extends GetxController {
   RxBool isLoading = false.obs;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<Map<String, dynamic>> addProduct(Map<String, dynamic> data) async {
+    try {
+      await firestore.collection("products").add(data);
+
+      return {
+        "error": false,
+        "message": "Berhasil menambah product.",
+      };
+    } catch (e) {
+      // Error general
+      print(e);
+      return {
+        "error": true,
+        "message": "Tidak dapat menambah product.",
+      };
+    }
+  }
 }
