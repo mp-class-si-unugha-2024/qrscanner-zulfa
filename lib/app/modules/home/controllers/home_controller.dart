d05b7c71290dfead86061a138129e7e8d5bcc5cf
zulfanurrifaat <zullfanurrifaat@gmail.com>
2024-03-19 01:38:22 +0700

    Export Pdf

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
@@ -13,87 +14,104 @@ class HomeController extends GetxController {
 
     pdf.addPage(
       pw.Page(
-        pageFormat: PdfPageFormat.a4,
-        build: (context) {
-          return pw.Column(children: [
-            pw.Center(
-              child: pw.Text(
-                "CATALOG PRODUCTS",
-                textAlign: pw.TextAlign.center,
-                style: const pw.TextStyle(
-                  fontSize: 24,
+          pageFormat: PdfPageFormat.a4,
+          build: (context) {
+            return pw.Column(children: [
+              pw.Center(
+                child: pw.Text(
+                  "CATALOG PRODUCTS",
+                  textAlign: pw.TextAlign.center,
+                  style: const pw.TextStyle(
+                    fontSize: 24,
+                  ),
                 ),
               ),
-            ),
-            pw.SizedBox(height: 20),
-            pw.Table(
-              border: pw.TableBorder.all(
-                color: PdfColor.fromHex("#000000"),
-                width: 2,
-              ),
-              children: [
-                pw.TableRow(
-                  children: [
-                    // No
-                    pw.Text(
-                      "No",
-                      style: pw.TextStyle(
-                        fontSize: 12,
-                        fontWeight: pw.FontWeight.bold,
-                      ),
-                    ),
-                    // Kode barang
-                    pw.Text(
-                      "Product Code",
-                      style: pw.TextStyle(
-                        fontSize: 12,
-                        fontWeight: pw.FontWeight.bold,
-                      ),
-                    ),
-                    // Nama barang
-                    pw.Text(
-                      "Product Name",
-                      style: pw.TextStyle(
-                        fontSize: 12,
-                        fontWeight: pw.FontWeight.bold,
-                      ),
-                    ),
-                    // qty
-                    pw.Text(
-                      "Quantity",
-                      style: pw.TextStyle(
-                        fontSize: 12,
-                        fontWeight: pw.FontWeight.bold,
-                      ),
-                    ),
-                    // qr code
-                    pw.Text(
-                      "Qr Code",
-                      style: pw.TextStyle(
-                        fontSize: 12,
-                        fontWeight: pw.FontWeight.bold,
-                      ),
-                    ),
-                  ],
+              pw.SizedBox(height: 20),
+              pw.Table(
+                border: pw.TableBorder.all(
+                  color: PdfColor.fromHex("#000000"),
+                  width: 2,
                 ),
-              ],
-            ),
-          ]);
-        },
-      ),
+                children: [
+                  pw.TableRow(
+                    children: [
+                      // NO
+                      pw.Padding(
+                        padding: const pw.EdgeInsets.all(20),
+                        child: pw.Text(
+                          "NO",
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                            fontWeight: pw.FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // kode nbarang
+                      pw.Padding(
+                        padding: const pw.EdgeInsets.all(20),
+                        child: pw.Text(
+                          "Product Code",
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                            fontWeight: pw.FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // nama barang
+                      pw.Padding(
+                        padding: const pw.EdgeInsets.all(20),
+                        child: pw.Text(
+                          "Product Name",
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                            fontWeight: pw.FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // qty
+                      pw.Padding(
+                        padding: const pw.EdgeInsets.all(20),
+                        child: pw.Text(
+                          "Quantity",
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                            fontWeight: pw.FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // Qr code
+                      pw.Padding(
+                        padding: const pw.EdgeInsets.all(20),
+                        child: pw.Text(
+                          "Qr Code",
+                          style: pw.TextStyle(
+                            fontSize: 12,
+                            fontWeight: pw.FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ]);
+          }),
     );
 
-    // simpan
-    Uint8List bytes = await pdf.save();
+    // Simpan PDF
+    final Uint8List bytes = await pdf.save();
 
-    // BUAT FILE KOSONG DI direktori
-    final dir = await getApplicationDocumentsDirectory();
-    final file = File('${dir.path}/myDocument.pdf');
+    // Dapatkan direktori aplikasi
+    final Directory directory = await getTemporaryDirectory();
 
-    // memasukan data bytes -> file kosong
+    // Buat file PDF
+    final String path = '${directory.path}/mydocument.pdf';
+    final File file = File(path);
+
+    // Tulis bytes ke file
     await file.writeAsBytes(bytes);
 
-    // open pdf
+    // Buka file PDF
     await OpenFile.open(file.path);
   }
 }
